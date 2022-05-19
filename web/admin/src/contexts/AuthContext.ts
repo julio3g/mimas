4e60aff76e9fbd0e1c1a816fdc339dcbd9eec500
Julio <juliocesar.orso@gmail.com>
2022-05-19 10:42:36 -0300

    add admin

---
+++ web/admin/src/contexts/AuthContext.ts
@@ -0,0 +1,92 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { setCookie, parseCookies, destroyCookie } from 'nookies';
+import Router from 'next/router';
+import { api } from '../services/apiClient';
+
+type User = {
+  email: string;
+  name: string;
+  phone: string;
+};
+
+type SignInCredentials = {
+  email?: string;
+  password: string;
+  phone?: string;
+};
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut: () => void;
+  user: User;
+  isAuthenticated: boolean;
+};
+interface AuthProviderProps {
+  children: ReactNode;
+}
+export const AuthContext = createContext({} as AuthContextData);
+let authChannel: BroadcastChannel;
+export function signOut() {
+  destroyCookie(undefined, 'mimas_next_access_token');
+  destroyCookie(undefined, 'mimas_next_refresh_token');
+  authChannel.postMessage('signOut');
+  Router.push('/');
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+  useEffect(() => {
+    authChannel = new BroadcastChannel('auth');
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case 'signOut':
+          signOut();
+          break;
+        default:
+          break;
+      }
+    };
+  }, []);
+  useEffect(() => {
+    const { mimas_next_access_token: token } = parseCookies();
+    if (token) {
+      api
+        .get('/users/profile')
+        .then((response) => {
+          const { name } = response.data;
+          setUser(name);
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
+  async function signIn({ email, password, phone }: SignInCredentials) {
+    try {
+      const response = await api.post('sessions', {
+        email,
+        phone,
+        password,
+      });
+      const { token, refresh_token, user: newLoggedUser } = response.data;
+      setCookie(undefined, 'mimas_next_access_token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+      setCookie(undefined, 'mimas_next_refresh_token', refresh_token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+      setUser(newLoggedUser);
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+      Router.push('/home');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return (
+    <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
---
+++ web/admin/src/services/api.ts
@@ -0,0 +1,87 @@
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
+import { signOut } from '../contexts/AuthContext';
+import { AuthTokenError } from './errors/AuthTokenError';
+
+let isRefreshing = false;
+let failedRequestsQueue = [];
+
+export function setupAPIClient(ctx = undefined) {
+  let cookies = parseCookies(ctx);
+
+  const api = axios.create({
+    // baseURL: 'https://api.app.construagil.net.br',
+    baseURL: 'http://localhost:3333',
+    headers: {
+      Authorization: `Bearer ${cookies['mimas_next_access_token']}`,
+    },
+  });
+
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      console.log(error?.response?.status);
+      if (error?.response?.status === 401) {
+        if (error.response.data?.message === 'Invalid token!') {
+          // // renovar o token
+          cookies = parseCookies();
+          const { mimas_next_refresh_token: refreshToken } = cookies;
+          const originalConfig = error.config;
+          if (!isRefreshing) {
+            isRefreshing = true;
+            api
+              .post('/refresh-token', { token: refreshToken })
+              .then((response) => {
+                const { token } = response.data;
+                setCookie(ctx, 'mimas_next_access_token', token, {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: '/',
+                });
+                setCookie(
+                  ctx,
+                  'mimas_next_refresh_token',
+                  response.data.refresh_token,
+                  {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/',
+                  },
+                );
+                api.defaults.headers['Authorization'] = `Bearer ${token}`;
+                failedRequestsQueue.forEach((request) =>
+                  request.onSuccess(token),
+                );
+                failedRequestsQueue = [];
+              })
+              .catch((err) => {
+                failedRequestsQueue.forEach((request) =>
+                  request.OnFailure(err),
+                );
+                failedRequestsQueue = [];
+                if (process.browser) signOut();
+              })
+              .finally(() => (isRefreshing = false));
+          }
+          return new Promise((resolve, reject) => {
+            failedRequestsQueue.push({
+              onSuccess: (token: string) => {
+                originalConfig.headers['Authorization'] = `Bearer ${token}`;
+                resolve(api(originalConfig));
+              },
+              OnFailure: (err: AxiosError) => reject(err),
+            });
+          });
+        } else {
+          // deslogar o usuário
+          if (process.browser) signOut();
+          else {
+            return Promise.reject(new AuthTokenError());
+          }
+        }
+      }
+      return Promise.reject(error);
+    },
+  );
+  return api;
+}
